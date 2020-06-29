9edc299f736f2d185c1f02a0db8b6f82f6d0ac2d
Jatin Katyal <jatin.katyal13@gmail.com>
2020-06-29 19:50:41 +0530

    setup react app inside the repo

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to React App !</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./frontend/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    chunkFilename: "[id].js",
+    publicPath: "",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]",
+              },
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [autoprefixer({})],
+            },
+          },
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/frontend/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
