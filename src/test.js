9cc66101fb9b830731bc97dbc1d9dd6681fb8c49
Jatin Katyal <jatin.katyal13@gmail.com>
2020-10-21 17:37:59 +0530

    add: graph certificate

--- src/test.js
+++ src/test.js
@@ -41,7 +41,7 @@ connectionPromise.then(conn => {
               end: 1548416990,
               // domain: "hellointern"
             },
-            template: 'cpp_live'
+            template: 'graph'
           },
           callback: 'evil.com'
         })
