60d31433954327218bc6ad2db85c1343f81e87a0
Vibhu Dujari <vdvibhu20@gmail.com>
2023-05-06 17:31:16 +0530

    use new html pdf

--- src/services/certificates-download/handlers.js
+++ src/services/certificates-download/handlers.js
@@ -8,44 +8,50 @@ const fs = require('fs')
 
 module.exports = {
   GENERATE: app => async (request, reply) => {
-    const layout = await app.mongo.db
-      .collection('layouts')
-      .findOne({
-        _id: app.transformStringIntoObjectId(request.body.layoutId || request.query.layoutId)
-      })
-
-    if (!layout) throw {
-      statusCode: 400,
-      message: 'Layout Not Found'
-    }
-
-    const destKeyName = `${v4()}.pdf`
-    const outPath = path.join(__dirname, '../../certification/' + v4() + ".pdf")
-    const document = {
-      template: layout.content,
-      context: {
-        ...request.body.substitutions,
-        pdfFileName: destKeyName
-      },
-      options: {
-        height: `${+layout.height || 408}px`,
-        width: `${+layout.width || 842}px`,
-        printBackground: true,
-        path: outPath  
+    try {
+      const layout = await app.mongo.db
+        .collection('layouts')
+        .findOne({
+          _id: app.transformStringIntoObjectId(request.body.layoutId || request.query.layoutId)
+        })
+  
+      if (!layout) throw {
+        statusCode: 400,
+        message: 'Layout Not Found'
       }
-    };
-
-    const template = Handlebars.compile(document.template);
-    const html = template(document.context);
-    if(layout.useHtmlPdf) {
-      await pdfv2.createPdf(html, document.options)
-    } else {
-      await pdf.createPdf(html, document.options);
+  
+      const destKeyName = `${v4()}.pdf`
+      const outPath = path.join(__dirname, '../../certification/' + v4() + ".pdf")
+      const document = {
+        template: layout.content,
+        context: {
+          ...request.body.substitutions,
+          pdfFileName: destKeyName
+        },
+        options: {
+          height: `${+layout.height || 408}px`,
+          width: `${+layout.width || 842}px`,
+          printBackground: true,
+          path: outPath  
+        }
+      };
+  
+      const template = Handlebars.compile(document.template);
+      const html = template(document.context);
+      if(layout.useHtmlPdf) {
+        await pdfv2.createPdf(html, document.options)
+      } else {
+        await pdf.createPdf(html, document.options);
+      }
+      const file = fs.readFileSync(outPath, 'utf8')
+      fs.unlinkSync(outPath)
+      reply.header('Content-Type', 'application/octet-stream')
+      reply.header('Content-Disposition', `attachment; filename="certificate.pdf"`)
+      reply.send(file)
     }
-    const file = fs.readFileSync(outPath, 'utf8')
-    fs.unlinkSync(outPath)
-    reply.header('Content-Type', 'application/octet-stream')
-    reply.header('Content-Disposition', `attachment; filename="certificate.pdf"`)
-    reply.send(file)
+    catch(err) {
+      reply.code(err.statusCode || 500)
+      .send(err)
+    }
   }
 }
--- src/utils/pdf-v2.js
+++ src/utils/pdf-v2.js
@@ -1,7 +1,10 @@
-const html2pdf = require('html-pdf')
+const html2pdf = require('html-pdf-node')
 
 module.exports = {
   createPdf: async function(html, options) {
+    return html2pdf.generatePdf({ content: html}, {...options, pageRanges: '1'})
+  },
+  createPdfv2: async function(html, options) {
     return new Promise((resolve, reject) => {
       html2pdf.create(html, options).toFile(options.path, function(err, res) {
         if (err) reject(err)
