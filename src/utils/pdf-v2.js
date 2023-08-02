bff6162c7db6442572e4aedb1925b7c074cafd8f
Vibhu Dujari <vdvibhu20@gmail.com>
2023-08-03 00:39:22 +0530

    naming pdf-v2

--- src/utils/pdf-v2.js
+++ src/utils/pdf-v2.js
@@ -1,10 +1,10 @@
-const html2pdf = require('html-pdf-node')
+const html2pdf = require('html-pdf')
 
 module.exports = {
-  createPdf: async function(html, options) {
+  createPdfv2: async function(html, options) {
     return html2pdf.generatePdf({ content: html}, {...options, pageRanges: '1'})
   },
-  createPdfv2: async function(html, options) {
+  createPdf: async function(html, options) {
     return new Promise((resolve, reject) => {
       html2pdf.create(html, options).toFile(options.path, function(err, res) {
         if (err) reject(err)
